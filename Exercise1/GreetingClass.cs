d9ebc3b7d2d6d751ba88c1f146a551b106c0c409
72Roman <27romario@gmail.com>
2019-11-21 22:53:44 +0200

    added checking for null and shouting response

--- Exercise1/GreetingClass.cs
+++ Exercise1/GreetingClass.cs
@@ -8,7 +8,22 @@ namespace Exercise1
     {
         public static string Greet(string name)
         {
+            if (String.IsNullOrEmpty(name))
+                return "Hello, my friend.";
+            if (IsStringInUpperCase(name))
+                return "HELLO " + name + '!';
             return "Hello, " + name + ".";
+        }
+        public static bool IsStringInUpperCase(string str)
+        {
+            if (String.IsNullOrEmpty(str))
+                return false;
+            foreach(var character in str)
+            {
+                if (!Char.IsUpper(character))
+                    return false;
+            }
+            return true;
         }
     }
 }
--- Exersice1Tests/UnitTest1.cs
+++ Exersice1Tests/UnitTest1.cs
@@ -15,11 +15,85 @@ namespace Exersice1Tests
         {
             // arrange
             string name = "Bob";
-            string actual = "Hello, Bob.";
+            string expected = "Hello, Bob.";
             // act
-            string result = GreetingClass.Greet(name);
+            string actual = GreetingClass.Greet(name);
             // assert
-            Assert.AreEqual(actual, result);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Greet_Null_HelloMyFriendReturned()
+        {
+            // arrange
+            string expected = "Hello, my friend.";
+            // act
+            string actual = GreetingClass.Greet(null);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Greet_EmptyString_HelloMyFriendReturned()
+        {
+            // arrange
+            string expected = "Hello, my friend.";
+            // act
+            string actual = GreetingClass.Greet("");
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void Greet_UpperCaseName_UpperCaseResponseReturned()
+        {
+            // arrange
+            string name = "JERRY";
+            string expected = "HELLO JERRY!";
+            // act
+            string actual = GreetingClass.Greet(name);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IsStringInUpperCase_UpperCaseString_TrueReturned()
+        {
+            // arrange
+            string name = "JERRY";
+            bool expected = true;
+            // act
+            bool actual = GreetingClass.IsStringInUpperCase(name);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IsStringInUpperCase_Null_FalseReturned()
+        {
+            // arrange
+            bool expected = false;
+            // act
+            bool actual = GreetingClass.IsStringInUpperCase(null);
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void IsStringInUpperCase_EmptyString_FalseReturned()
+        {
+            // arrange
+            bool expected = false;
+            // act
+            bool actual = GreetingClass.IsStringInUpperCase("");
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        [TestCase("jerry")]
+        [TestCase("12~++-")]
+        [TestCase("Jerry")]
+        public void IsStringInUpperCase_NoUpperCaseString_FalseReturned(string name)
+        {
+            bool expected = false;
+            // act
+            bool actual = GreetingClass.IsStringInUpperCase(name);
+            // assert
+            Assert.AreEqual(expected, actual);
         }
     }
 }
